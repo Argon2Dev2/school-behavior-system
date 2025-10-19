9198b3ac2795a8f44fc9bdc229b5d039166dee3c
Argon2Dev2 <testforthing0010@gmail.com>
2025-10-19 16:38:13 +0300

    Create const.ts

---
+++ shared/const.ts
@@ -0,0 +1,3 @@
+export const COOKIE_NAME = "session";
+export const APP_TITLE = import.meta.env.VITE_APP_TITLE || "نظام إدارة السلوك المدرسي";
+export const APP_LOGO = import.meta.env.VITE_APP_LOGO || "/icon-192.png";
